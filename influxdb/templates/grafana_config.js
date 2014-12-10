7b69ceafbb3910f7f76cb610f3074295e7c962ea
vforgione <v.forgione@gmail.com>
2014-12-10 15:24:33 -0600

    fixed local deploy

--- influxdb/templates/grafana_config.js
+++ influxdb/templates/grafana_config.js
@@ -1,56 +1,45 @@
-define(['settings'], function(Settings) {
-
+define(['settings'], function (Settings) {
 
   return new Settings({
 
-      /* Data sources
-      * ========================================================
-      */
-
-      datasources: {
-        grafana: {
-          type: 'influxdb',
-          url: "http://localhost:8086/db/grafana",
-          username: 'grafana',
-          password: '{{ INFLUXDB_GRAFANA_PASSWORD }}',
-          grafanaDB: true
-        }
+    datasources: {
+      oniontv: {
+        type: 'influxdb',
+        url: "http://influx.local:8086/db/oniontv",
+        username: 'root',
+        password: 'root',
+        default: true
       },
 
-      /* Global configuration options
-      * ========================================================
-      */
-
-      // specify the limit for dashboard search results
-      search: {
-        max_results: 100
-      },
-
-      // default home dashboard
-      default_route: '/dashboard/file/default.json',
-
-      // set to false to disable unsaved changes warning
-      unsaved_changes_warning: true,
-
-      // set the default timespan for the playlist feature
-      playlist_timespan: "1m",
-
-      // If you want to specify password before saving, please specify it below
-      // The purpose of this password is not security, but to stop some users from accidentally changing dashboards
-      admin: {
-        password: ''
-      },
-
-      // Change window title prefix from 'Grafana - <dashboard title>'
-      window_title_prefix: '',
-
-      // Add your own custom panels
-      plugins: {
-        // list of plugin panels
-        panels: [],
-        // requirejs modules in plugins folder that should be loaded for example custom datasources
-        dependencies: []
+      grafana: {
+        type: 'influxdb',
+        url: "http://influx.local:8086/db/grafana",
+        username: 'root',
+        password: 'root',
+        grafanaDB: true
       }
+    },
 
-    });
+    search: {
+      max_results: 10000
+    },
+
+    default_route: '/dashboard/file/default.json',
+
+    unsaved_changes_warning: true,
+
+    playlist_timespan: "1m",
+
+    admin: {
+      password: ''
+    },
+
+    window_title_prefix: 'Grafana - ',
+
+    plugins: {
+      panels: [],
+      dependencies: []
+    }
+
+  });
 });
